4243d6813c0911bba2335038d73dd8ab4ff9b8e8
Nikita Bellini <nikitabellini@MacBook-Air-di-Nikita.local>
2026-01-17 11:09:26 +0100

    Fix Range docstring

--- mosaico-sdk-py/src/mosaicolabs/models/sensors/range.py
+++ mosaico-sdk-py/src/mosaicolabs/models/sensors/range.py
@@ -14,22 +14,22 @@ from ..serializable import Serializable
 
 class Range(Serializable, HeaderMixin, VarianceMixin):
     """
-    Represents a range measurement. The internal representation is always stored
-    in **meters (m)**.
-
-    A range measurement defines a valid distance interval between a minimum and
-    maximum value, together with the measured range, the sensor field of view,
-    and the radiation type used by the sensor.
+    Represents a range measurement that defines a valid distance interval between the minimum and the maximum value.
+    This with also the field of view, the radiation type and the range value.
+    The internal representation is always stored in **meters (m)**.
 
     Parameters:
-        radiation_type (int): Identifier of the radiation type used by the sensor.
-        field_of_view (float):Arc angle, in **radians**, over which the distance reading is valid.
-        min_range (float): Minimum valid range value in **meters (m)**. A fixed-distance measurement
-            is represented by setting `min_range` equal to `max_range`.
-        max_range (float): Maximum valid range value in **meters (m)**. A fixed-distance measurement
-            is represented by setting `min_range` equal to `max_range`.
-        range (float): Measured range value in **meters (m)**. The value **must** lie between
-            `min_range` and `max_range`, inclusive.
+        radiation_type (int): Which type of radiation the sensor used.
+        field_of_view (float): The arc angle, in **Radians (rad)**, over which the distance reading is valid.
+            When using the constructor directly, the value **must** be provided in Radians.
+        min_range (float): Minimum range value in **Meters (m)**. Fixed distance means that the minimum range
+            must be equal to the maximum range. When using the constructor directly, the value **must** be
+            provided in Meters.
+        max_range (float): Maximum range value in **Meters (m)**. Fixed distance means that the minimum range
+            must be equal to the maximum range. When using the constructor directly, the value **must** be
+            provided in Meters.
+        range (float): Range value in **Meters (m)**. When using the constructor directly, the value **must** be
+            provided in Meters.
     """
 
     # --- Schema Definition ---
